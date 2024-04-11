9c9ed724879e9ca88a44cbb382df8d1d8763f753
JUDHI3134 <judhistirbehera241@gmail.com>
2024-04-11 11:39:34 +0530

    changes

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+let course = document.querySelector(".detail-course")
+let button = document.querySelector(".btn");
+
+button.addEventListener('mouseover',()=>{
+    course.style.display = "block"
+})
+button.addEventListener('click',()=>{
+    course.style.display = "none"
+})
